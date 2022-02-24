35245da43311c8355d36c51222a9632e6258ec6c
Tenjizen <92018813+Tenjizen@users.noreply.github.com>
2022-02-24 10:53:51 +0100

    .

--- Assets/Scripts/Player/Direction.cs
+++ Assets/Scripts/Player/Direction.cs
@@ -4,16 +4,19 @@ public class Direction : MonoBehaviour
 {
     [SerializeField] private LineRenderer lr;
 
+    public GameObject ball;
+
+
     private void Awake()
     {
         lr = GetComponent<LineRenderer>();
     }
 
-    public void RenderLine(Vector3 startMousePos, Vector3 endMousePos)
+    public void RenderLine(Vector3 ball.transform.position, Vector3 endMousePos)
     {
         lr.positionCount = 2;
         Vector3[] points = new Vector3[2];
-        points[0] = startMousePos;
+        points[0] = ball.transform.position;
         points[1] = endMousePos;
 
         lr.SetPositions(points);
