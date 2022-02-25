857843fad6a236c7856d25958ee8593fab97783c
Tenjizen <nicolaslepers176@gmail.com>
2022-02-25 02:54:52 +0100

    f bis

--- Assets/Scripts/Ennemis/KillEnnemis.cs
+++ Assets/Scripts/Ennemis/KillEnnemis.cs
@@ -8,7 +8,7 @@ public class KillEnnemis : MonoBehaviour
 
     public SpriteRenderer SpriteRenderer;
     [SerializeField] private Sprite spriteEnnemisDie;
-
+    public Animator animator;
 
     private bool collisionWithEnnemi = false;
 
@@ -16,8 +16,10 @@ public class KillEnnemis : MonoBehaviour
     {
         if (collisionWithEnnemi && Input.GetKeyDown(KeyCode.E))
         {
+            animator.SetBool("IsAttacking", true);
             SpriteRenderer.sprite = spriteEnnemisDie;
             transform.Find("FOV").gameObject.SetActive(false);
+            StartCoroutine(AnimAttacking(1.5f));
         }
     }
     private void OnTriggerEnter2D(Collider2D col)
@@ -36,4 +38,9 @@ public class KillEnnemis : MonoBehaviour
 
     }
 
+    public IEnumerator AnimAttacking(float n)
+    {
+        yield return new WaitForSeconds(n);
+        animator.SetBool("IsAttacking", false);
+    }
 }
--- Assets/Scripts/Player/Line.cs
+++ Assets/Scripts/Player/Line.cs
@@ -1,3 +1,4 @@
+using System.Collections;
 using UnityEngine;
 
 public class Line : MonoBehaviour
@@ -15,6 +16,9 @@ public class Line : MonoBehaviour
     private Vector2 force;
     private Vector3 startMousePos;
     private Vector3 endMousePos;
+
+    public Animator animator;
+
 
     public static Line Instance;
 
@@ -73,37 +77,50 @@ public class Line : MonoBehaviour
         {
             if (Input.GetMouseButtonDown(0))
             {
+                Debug.Log("gdgjdfklghkljdg");
+                animator.SetBool("StartLancer", true);
                 startMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
                 startMousePos.z = 15;
                 Debug.Log(startMousePos);
             }
             if (Input.GetMouseButton(0))
             {
+                
+
                 Vector3 currentPoint = cam.ScreenToWorldPoint(Input.mousePosition);
                 currentPoint.z = 15;
                 dir.RenderLine(startMousePos, currentPoint);
             }
             if (Input.GetMouseButtonUp(0))
             {
+                Debug.Log("ghjfkhgkjghjklhsgldfh BIS");
+                animator.SetBool("EndLancer", true);
 
+                StartCoroutine(AnimLancer(0.2f));
 
-                endMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
-                endMousePos.z = 15;
-                Debug.Log(endMousePos);
-
-                Vector2 dist = (startMousePos - endMousePos).normalized;
-
-                force = new Vector2(
-                    Mathf.Clamp(dist.x, minPower.x, maxPower.x),
-                    Mathf.Clamp(dist.y, minPower.y, maxPower.y)
-                    );
-
-                rb.AddForce(force * power, ForceMode2D.Impulse);
-                dir.EndLine();
-                CircleCollider.Instance.countShot++;
+                
             }
         }
     }
+    public IEnumerator AnimLancer(float n)
+    {
+        yield return new WaitForSeconds(n);
+        Debug.Log("gklfdqsghjsvjnkjdgnfhvksdfgj THIRD");
+        animator.SetBool("EndLancer", false); 
+        endMousePos = cam.ScreenToWorldPoint(Input.mousePosition);
+        endMousePos.z = 15;
+        Debug.Log(endMousePos);
 
+        Vector2 dist = (startMousePos - endMousePos).normalized;
+
+        force = new Vector2(
+            Mathf.Clamp(dist.x, minPower.x, maxPower.x),
+            Mathf.Clamp(dist.y, minPower.y, maxPower.y)
+            );
+
+        rb.AddForce(force * power, ForceMode2D.Impulse);
+        dir.EndLine();
+        CircleCollider.Instance.countShot++;
+    }
 
 }
