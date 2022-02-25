f5d07dbc131ee35481513151e31bd182d75119cd
Tenjizen <nicolaslepers176@gmail.com>
2022-02-25 03:52:55 +0100

    .

---
+++ Assets/Scripts/Menu/ModifSpritButton.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ModifSpritButton : MonoBehaviour
+{
+    public Button button;
+    public Image imageTest;
+    public Sprite Sprite;
+    public Sprite OldSprite;
+
+    public void MouseOver()
+    {
+        button.image = imageTest;
+        imageTest.sprite = Sprite;
+    }
+    public void MouseExit()
+    {
+        button.image = imageTest;
+        imageTest.sprite = OldSprite;
+    }
+}
